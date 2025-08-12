398f9d5ec00956eac6e3beb246edf49c8e76e01f
GauravYadav-19 <gauravyadavbkn@gmail.com>
2025-08-12 16:24:15 +0530

    data types

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,20 @@
+//alert(3+3) //we are using nodejs, not browser
+
+let age = 19
+// to check the datatype we use {typeof}
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+/*  PRIMITIVE DATATYPES
+number => 2 to the power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => no value assigned 
+symbol => used for uniqueness
+ 
+object
+
+*/

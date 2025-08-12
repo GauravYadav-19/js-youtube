6c1f98fd4ff686a751ae3622bea86e0147294acd
GauravYadav-19 <gauravyadavbkn@gmail.com>
2025-08-12 16:06:09 +0530

    lecture 1

---
+++ basics_01/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 112233;
+let accountEmail = "gaurav@google.com";
+var accountpassword = "22112211";
+accountCity = "Bikaner";
+
+accountEmail = "abc@google.com";
+accountpassword = "1234567";
+accountCity = "Chandigarh"
+
+// console.table([accountId,accountEmail,accountpassword,accountCity]);
+//console.log(typeof(accountEmail));
+
+/* 
+IMPORTANT: prefer not to use var bcoz of issue in block scope and functional scope
+           and we can create variables without using var,const,let but this is a really bad practice 
+*/

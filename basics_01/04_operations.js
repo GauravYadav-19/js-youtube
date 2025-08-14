0fcd413f6c96994dd18a41ec3a7b5fd39202a6fd
GauravYadav-19 <gauravyadavbkn@gmail.com>
2025-08-14 14:54:30 +0530

    operations

---
+++ basics_01/04_operations.js
@@ -0,0 +1,22 @@
+let value = 3
+let negVal = -value
+//console.log(negVal)
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2**3) //2 to the power 3
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "gaurav"
+//console.log(str1+str2)
+
+// console.log(1+"2")
+// console.log("1"+"2"+2)
+// console.log(1+2+"2")    // for more detail visit tc39.es and study ecma script and type conversion for its rules
+
+// console.log(+true)
+// console.log("")
+// console.log(+"")
